a7990f83ba33698d8d68b249eba9e7173b1d300c
Logan Goldberg <goldberg@grinnell.edu>
2017-04-24 14:24:26 -0500

    Added sidebar, idk how

--- src/vis.js
+++ src/vis.js
@@ -1,26 +1,26 @@
 // Define dimensions for visualization
-var width  = 960;
-var height = 500;
+var width  = 1800;
+var height = 800;
 
 // Set up a projection for England which is centered and
 // scaled appropriately
 var projection = d3.geoAlbers()
     .center([0, 55.4])
-    .rotate([4.4, 0])
+    .rotate([4.6, 0])
     .parallels([50, 60])
     .scale(1200 * 5)
-    .translate([width / 2, height / 2]);
+    .translate([width / 2, height / 4]);
 
 var path = d3.geoPath().projection(projection);
 
 // Add an SVG element
-var svg = d3.select("body").append("svg")
+var svg = d3.select("#map").append("svg")
     .attr("width", width)
     .attr("height", height);
 
 // Queue a sequence of requests for drawing the map
 d3.queue()
-    .defer(d3.json, '../data/uk.json')
+    .defer(d3.json, '../data/uk_other.json')
     .await(function(error, uk) {
         if (error) {
             console.error(error);
@@ -30,13 +30,13 @@ d3.queue()
 
         // Draw land
         svg.append('path')
-            .datum(topojson.feature(uk, uk.objects.wards))
+            .datum(topojson.feature(uk, uk.objects.lad))
             .attr("class", "land")
             .attr("d", path);
 
         svg.append("path")
-            .datum(topojson.mesh(uk, uk.objects.wards,
+            .datum(topojson.mesh(uk, uk.objects.lad,
                                  function(a, b) { return a !== b; }))
-            .attr("class", "ward-boundary")
+            .attr("class", "lad-boundary")
             .attr("d", path);      
     });
